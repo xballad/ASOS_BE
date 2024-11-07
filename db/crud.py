ec80c05bca2afddd82ffa5b990ad628f4f8d1b06
Ivona <xbiresi@stuba.sk>
2024-11-07 22:36:08 +0100

    initial commit

---
+++ db/crud.py
@@ -0,0 +1,86 @@
+
+from sqlalchemy.orm import Session
+from user import User
+
+
+def create_user(db: Session, name: str, last_name: str, username: str, password: str, email: str):
+    db_user = User(name=name, last_name=last_name, username=username, password=password, email=email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def update_user_name(db: Session, user_id: int, name: str):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db_user.name = name
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    return None
+
+
+def update_user_last_name(db: Session, user_id: int, last_name: str):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db_user.last_name = last_name
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    return None
+
+
+def update_user_email(db: Session, user_id: int, email: str):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db_user.email = email
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    return None
+
+
+def update_user_username(db: Session, user_id: int, username: str):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db_user.username = username
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    return None
+
+
+def update_user_password(db: Session, user_id: int, password: str):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db_user.password = password
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    return None
+
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+        return db_user
+    return None
---
+++ db/dbTest.py
@@ -0,0 +1,37 @@
+
+from db import SessionLocal, engine
+import user
+from crud import create_user, get_users, update_user_name, update_user_last_name, update_user_email, \
+    update_user_username, update_user_password, delete_user, get_user_by_id
+
+# Run tests
+
+# Creating table
+user.Base.metadata.create_all(bind=engine)
+db = SessionLocal()
+
+# Create a new user
+new_user = create_user(db, name="John", last_name="Doe", username="johndoe3", password="password123",
+                       email="john.doe3@example.com")
+print(f"Created User: {new_user}")
+
+# Get the user by ID
+user_by_id = get_user_by_id(db, new_user.id)
+print(f"User by ID: {user_by_id}")
+
+# Update the user's name
+updated_user_name = update_user_name(db, new_user.id, name="John Updated")
+print(f"Updated Name: {updated_user_name}")
+
+# Update the user's last name
+updated_user_last_name = update_user_last_name(db, new_user.id, last_name="Doe Updated")
+print(f"Updated Last Name: {updated_user_last_name}")
+
+# Get all users
+all_users = get_users(db)
+print(f"All Users: {all_users}")
+
+# Delete the user
+deleted_user = delete_user(db, new_user.id)
+print(f"Deleted User: {deleted_user}")
+
